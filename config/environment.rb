d9af59ff12f479b63bbf2c50225a05c3089c5fdb
John Parsons <jparsons@klondikefive.com>
2010-08-28 11:01:45 -0400

    rspec

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,9 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem 'easy_roles', :source => 'http://gemcutter.org'
+  config.gem "rspec", :lib => false, :version => ">= 1.3.0"
+  config.gem "rspec-rails", :lib => false, :version => ">= 1.3.2"
+  config.gem "mocha"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
